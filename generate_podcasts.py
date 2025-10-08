1bb924af2105172115f015bbf139ac757798d270
ZigAcademy <ZigAcademy@zig.nl>
2025-10-08 14:55:44 +0200

    Update generate_podcasts.py

--- generate_podcasts.py
+++ generate_podcasts.py
@@ -2,25 +2,21 @@ import os
 import json
 
 def generate_podcast_json(base_path='.'):
-    # Zoek alle mappen in de root, behalve verborgen en systeemmappen
-    podcast_dirs = [
-        d for d in os.listdir(base_path)
-        if os.path.isdir(os.path.join(base_path, d)) and not d.startswith('.') and d != '.git'
-    ]
-
-    # Maak een lijst van podcasts met index.html
     podcasts = []
-    for folder in podcast_dirs:
-        index_path = os.path.join(folder, 'index.html')
-        if os.path.exists(index_path):
-            podcasts.append({
-                "title": folder.replace('-', ' ').replace('_', ' '),
-                "path": f"{folder}/index.html"
-            })
+    for root, dirs, files in os.walk(base_path):
+        for folder in dirs:
+            folder_path = os.path.join(root, folder)
+            index_path = os.path.join(folder_path, 'index.html')
+            if os.path.isfile(index_path):
+                relative_path = os.path.relpath(index_path, base_path)
+                podcasts.append({
+                    "title": folder.replace('-', ' ').replace('_', ' '),
+                    "path": relative_path.replace('\\', '/')
+                })
 
-    # Schrijf naar podcasts.json
     with open('podcasts.json', 'w', encoding='utf-8') as f:
         json.dump(podcasts, f, indent=2, ensure_ascii=False)
 
-# Voer het script uit
+    print(f"Generated podcasts.json with {len(podcasts)} entries.")
+
 generate_podcast_json()
